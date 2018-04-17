2e74c44e23abe755b0e47eb5cd96c0f18d68ff75
Walter Maldonado Jr <walter@agroestat.com.br>
2018-04-17 17:42:24 -0300

    Calculation fix Bug fixes Layout improvements About tab History chart

--- app/src/main/java/br/com/maldonado/instantet0/Tab3Fragment.java
+++ app/src/main/java/br/com/maldonado/instantet0/Tab3Fragment.java
@@ -1,21 +1,20 @@
 package br.com.maldonado.instantet0;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * @author Walter Maldonado Jr.
- */
 public class Tab3Fragment extends Fragment {
 
     public Tab3Fragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +22,7 @@ public class Tab3Fragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab3, container, false);
